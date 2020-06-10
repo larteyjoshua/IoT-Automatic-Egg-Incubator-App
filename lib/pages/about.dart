95716a2a2bc059807ea424e753164a54cfd0467c
Akora Ing. DKB <debrahkwesibuabeng2@gmail.com>
2020-06-10 18:21:20 +0000

    Add headings to CSV file

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -35,7 +35,6 @@ class Row {
 class _MyAboutPageState extends State<About> {
   final Color primaryColor = Color(0xff99cc33);
   //DatabaseHelper databaseHelper = DatabaseHelper();
-  final List<Row> rows = [];
 
   @override
   Widget build(BuildContext context) {
@@ -85,6 +84,13 @@ class _MyAboutPageState extends State<About> {
 
     getCsv() async {
       DatabaseHelper().getReadalldataList().then((data) async {
+        List<List<dynamic>> rowdata = List<List<dynamic>>();
+
+        // Add headers for various columns
+        rowdata.add(['ID', 'Time', 'Temperature', 'Humidity']);
+
+        final List<Row> rows = [];
+
         for (Map map in data) {
           rows.add(Row(
               id: map['id'],
@@ -92,7 +98,7 @@ class _MyAboutPageState extends State<About> {
               temperature: double.tryParse('${map['temperature']}'),
               humidity: double.tryParse('${map['humidity']}')));
         }
-        List<List<dynamic>> rowdata = List<List<dynamic>>();
+
         for (int i = 0; i < rows.length; i++) {
 //row refer to each column of a row in csv file and rows refer to each row in a file
           List<dynamic> rowconvert = List();
@@ -115,7 +121,10 @@ class _MyAboutPageState extends State<About> {
 
         print(directory.path);
 
-        File f = new File('${directory.path}/Incubator data.csv');
+        String moment =
+            "${DateTime.now().year}.${DateTime.now().month}.${DateTime.now().day}_${DateTime.now().hour}.${DateTime.now().minute}";
+
+        File f = new File('${directory.path}/Incubator Data_$moment.csv');
         String incubatorDatabse = const ListToCsvConverter().convert(rowdata);
         await f.writeAsString(incubatorDatabse);
         print('data downloaded');
@@ -152,7 +161,8 @@ class _MyAboutPageState extends State<About> {
                 children: <Widget>[
                   Text(
                       'This will download app database into the download folder.'),
-                  Text(' Are you sure?'),
+                  SizedBox(height: 16),
+                  Text('Are you sure?'),
                 ],
               ),
             ),
